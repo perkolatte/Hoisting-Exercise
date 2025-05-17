8336494877001e77bd01ab9b064a038543e48a99
Ian Isaacson <ian_isaacson@yahoo.com>
2025-05-17 13:58:46 -0700

    Exercise complete!

--- time-travel.js
+++ time-travel.js
@@ -1,19 +1,53 @@
 /* Task 1: Declare a Destination Variable */
-// TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
+// TODO:
+// Use `let` to declare a variable named `destination` and
+// assign it the value `"Ancient Egypt"`.
+// Print the destination to the console.
+
+let destination = "Ancient Egypt";
+console.log(destination);
 
 /* Task 2: Change the Destination */
-// TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
+// TODO:
+// Now, change the `destination` variable to `"Medieval Europe"`.
+// Print the new destination to the console.
+
+destination = "Medieval Europe";
+console.log(destination);
 
 /* Task 3: Declare a Constant Travel Date */
-// TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
+// TODO:
+// Use `const` to declare a variable named `travelDate` and
+// set it to `"2024-03-15"`.
+// Try to change the `travelDate` to another value and
+// observe and explain what happens as a comment.
+
+const travelDate = "2024-03-15";
+// const travelDate = "2024-03-16";
+
 /*
  * Observations:
  * TODO: Explain here.
+ *   Will throw a SytaxError because `const` variables cannot be reassigned.
  */
 
 /* Task 4: Experiment with Variable Hoisting */
-// TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
+// TODO:
+// Before declaring any variable,
+// try to print a variable named `timeMachineModel` to the console.
+// Then, declare `timeMachineModel` using `var` and
+// assign it the value `"T-800"`.
+// Observe and explain what happens as a comment.
+
+console.log(timeMachineModel);
+var timeMachineModel = "T-800";
+
 /*
  * Observations:
  * TODO: Explain here.
+ *  Before declaring timeMachineModel, this will throw a ReferenceError because timeMachineModel is not defined.
+ *
+ *  After declaring it with var, it will print `undefined` because of hoisting,
+ *      as the variable will be declared, but not yet assigned a value.
+ *
  */
